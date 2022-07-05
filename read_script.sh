c8efe0797e50eb133b6432c0c71751d26de3bba3
etoten <nick@uissoft.com>
2022-07-05 22:26:27 +0100

    added support for old device to read_script.sh file

--- read_script.sh
+++ read_script.sh
@@ -4,8 +4,29 @@
 log_user 0
 #
 set timeout 4
-#
-spawn gatttool -b 94:3c:c6:dd:42:ce -I
+# MAC address of RD200 device
+set MAC_A "94:3c:c6:dd:42:ce"
+# 1 for new RD200 device, 0 for old RD200 device
+set DEVICE_TYPE 1
+
+#command sent to get data
+set COMMAND  50
+
+##New device:
+#send 0x002a -> notification receive on: 0x002c
+if {$DEVICE_TYPE == "1"} {
+      set SEND_HANDLE  0x002a
+      set RECIEVE_HANDLE  0x002c
+}
+
+##Old device:
+##send 0x000b -> notification receive on: 0x000d
+if {$DEVICE_TYPE ==  "0" } {
+      set SEND_HANDLE  0x000b
+      set RECIEVE_HANDLE  0x000d
+}
+
+spawn gatttool -b $MAC_A -I
 #
 match_max 100000
 #
@@ -20,19 +41,19 @@ while (1) {
 #
 send -- "mtu 507\r"
 expect "MTU was exchanged successfully" {
-
     sleep 1
-    send -- "char-write-cmd 0x002a 50\r"
+    send -- "char-write-cmd $SEND_HANDLE $COMMAND\r"
+
 
     set systemTime [clock seconds]
     puts "Time [clock format $systemTime -format %Y-%m-%dT%H:%M:%S]"
     set message1 ""
 
     expect {
-             -re "Notification handle = 0x002c value: (.+\n)" {
-              set message1 ${message1}$expect_out(1,string)
-              exp_continue
-        }
+               -re "Notification handle = $RECIEVE_HANDLE value: (.+\n)" {
+		  set message1 ${message1}$expect_out(1,string)
+                  exp_continue
+                }
       }
       puts "$message1"
   }
